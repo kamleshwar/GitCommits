4018bc4445fd954ea44078d1c1b11cd35114ddfe
Kamleshwar <kamleshwar.dhuria@gmail.com>
2020-11-19 22:57:46 -0500

    chore: set Commits model

---
+++ GitCommits/GitCommits/CommitsModel.swift
@@ -0,0 +1,15 @@
+//
+//  CommitsModel.swift
+//  GitCommits
+//
+//  Created by Kamleshwar on 11/19/20.
+//  Copyright © 2020 Kamleshwar. All rights reserved.
+//
+
+import Foundation
+
+struct Commits {
+    let message: String
+    let author: String
+    let sha: String
+}
