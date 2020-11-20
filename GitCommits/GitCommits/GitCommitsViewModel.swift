5ea0df21639496320fb610f82181f83f3e852acb
Kamleshwar <kamleshwar.dhuria@gmail.com>
2020-11-19 22:41:08 -0500

    chore: implemented ViewModel to call get commits api

---
+++ GitCommits/GitCommits/GitCommitsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  GitCommitsViewModel.swift
+//  GitCommits
+//
+//  Created by Kamleshwar on 11/19/20.
+//  Copyright © 2020 Kamleshwar. All rights reserved.
+//
+
+import Foundation
+
+class GitCommitsViewModel {
+    
+    public func fetchCommits(completion: @escaping (Result<[String], Error>) -> Void) {
+        // make API Call
+    }
+}
--- GitCommits/GitCommits/ViewController.swift
+++ GitCommits/GitCommits/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/*      Requirements
+ 1. Create a free GitHub account - Done
+ 2. Create a new GitHub repository - Done
+ 3. Create a mobile app using Swift that accomplishes the following
+    a) Connects to the GitHub API;
+    b) Uses that API to retrieve the most recent commits
+    c) Displays those commits in a list with the author, commit hash, and commit message.
+ */
 
+class ViewController: UIViewController {
+    let viewModel = GitCommitsViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewModel.fetchCommits(completion: { _ in })
     }
-
-
 }
 
